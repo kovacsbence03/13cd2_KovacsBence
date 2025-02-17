7e5e57692e4d8b25db23073bc9f59fb6cb9a619a
kovacsbence03 <kovben077@hengersor.hu>
2025-02-17 14:31:56 +0100

    select_használata

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 const express = require("express")
 const app = express()
-const mysql= require("mysql")
+const mysql = require("mysql")
 const cors = require("cors")
 const bodyParser = require("body-parser");
 
 app.use(cors())
-app .use(bodyParser.json());
- 
-app.listen(3000, () => {
-    console.log("A szerver a 3000 porton fut");
-}); 
+app.use(bodyParser.json());
+
 const db = mysql.createConnection(
-    {      
-     user:"root",
-    host:"localhost",
-    port: 3307,
-    password:"",
-    database:"teliolimpia",
+    {
+        user: "root",
+        host: "localhost",
+        port: 3307,
+        password: "",
+        database: "teliolimpia",
     }
 );
 //a szerver futásának az ellenőrzése
@@ -24,4 +21,23 @@ const db = mysql.createConnection(
 app.get("/", (req, res) => {
     res.send("Fut a backend!");
 })
- 
+
+app.get("/v", (req, res) => {
+    const sql = "SELECT * FROM `versenyzok`";
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result)
+    })
+})
+
+app.get("/v3", (req, res) => {
+    const sql = "SELECT * FROM `versenyzok` where ID = 3";
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result)
+    })
+})
+
+app.listen(3000, () => {
+    console.log("A szerver a 3000 porton fut");
+}); 
